5378798518e914965cc94051253d9614c2d42cda
AlongsCode <l851601549@gmail.com>
2022-12-21 19:18:56 +0800

    取文件尺寸修仅使用stl，抛弃win32api

--- src/磁盘操作/取文件尺寸.cpp
+++ src/磁盘操作/取文件尺寸.cpp
@@ -1,55 +1,53 @@
-﻿
-
-#ifdef _WIN32
-#include<string>
-#include<windows.h>
-using namespace std;
-KrnlnApi long long 取文件尺寸(const string& filename)
-{
-	WIN32_FIND_DATAA inf;
-	const HANDLE handle = ::FindFirstFileA(filename.c_str(), &inf);
-	if (handle != INVALID_HANDLE_VALUE) {
-		::FindClose(handle);
-		return (((INT64)inf.nFileSizeHigh << 32) | (INT64)inf.nFileSizeLow);
-	}
-	return -1;
-}
-KrnlnApi long long 取文件尺寸(const wstring& filename) {
-	WIN32_FIND_DATAW inf;
-	const HANDLE handle = ::FindFirstFileW(filename.c_str(), &inf);
-	if (handle != INVALID_HANDLE_VALUE) {
-		::FindClose(handle);
-		return (((INT64)inf.nFileSizeHigh << 32) | (INT64)inf.nFileSizeLow);
-	}
-	return -1;
-}
-#else
-#include <fstream>
+﻿//
+//
+//#ifdef _WIN32
+//#include<string>
+//#include<windows.h>
+//using namespace std;
+//KrnlnApi long long 取文件尺寸(const string& filename)
+//{
+//	WIN32_FIND_DATAA inf;
+//	const HANDLE handle = ::FindFirstFileA(filename.c_str(), &inf);
+//	if (handle != INVALID_HANDLE_VALUE) {
+//		::FindClose(handle);
+//		return (((INT64)inf.nFileSizeHigh << 32) | (INT64)inf.nFileSizeLow);
+//	}
+//	return -1;
+//}
+//KrnlnApi long long 取文件尺寸(const wstring& filename) {
+//	WIN32_FIND_DATAW inf;
+//	const HANDLE handle = ::FindFirstFileW(filename.c_str(), &inf);
+//	if (handle != INVALID_HANDLE_VALUE) {
+//		::FindClose(handle);
+//		return (((INT64)inf.nFileSizeHigh << 32) | (INT64)inf.nFileSizeLow);
+//	}
+//	return -1;
+//}
+//
+//
+//
+//
+//
+//
+//#else
+#include <filesystem>
 KrnlnApi long long 取文件尺寸(const std::string& filename) {
-	std::ifstream fp(filename, std::ios::binary);
-	if (!fp.is_open()) {
+	if (!std::filesystem::exists(filename)) {
 		return -1;
 	}
 
-	fp.seekg(0, std::ios::end);
-	long long size = fp.tellg();
-	fp.close();
-	return size;
+	return std::filesystem::file_size(filename);
 }
 
 KrnlnApi long long 取文件尺寸(const std::wstring& filename) {
-	std::wifstream fp(filename, std::ios::binary);
-	if (!fp.is_open()) {
+	if (!std::filesystem::exists(filename)) {
 		return -1;
 	}
 
-	fp.seekg(0, std::ios::end);
-	long long size = fp.tellg();
-	fp.close();
-	return size;
+	return std::filesystem::file_size(filename);
 }
 
-#endif 
+//#endif 
 
 
 
