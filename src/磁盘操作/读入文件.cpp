187042e9495ea090c27926682594cf7c7205926a
AlongsCode <l851601549@gmail.com>
2022-12-23 05:24:20 +0800

    移除内存映射文件的内存拷贝，使用文件流来完成读入文件命令

--- src/磁盘操作/读入文件.cpp
+++ src/磁盘操作/读入文件.cpp
@@ -1,52 +1,7 @@
 ﻿#include <fstream>  
 #include <vector>   
-#ifndef _MSVC_LANG
-#undef _MSVC_LANG
-#define _MSVC_LANG __cplusplus
-#endif
 
-#define  CANUSEMAPFILE 999999//c++20不行，什么时候可以了直接替换
-#if _MSVC_LANG > CANUSEMAPFILE
-#include <memory_resource>  // 内存映射
-KrnlnApi std::vector<unsigned char> 读入文件(const std::string& filename) {
-	// 创建一个内存资源来映射文件
-	std::pmr::monotonic_buffer_resource mem_res;
-	std::pmr::map_file(filename, mem_res);
-	// 从内存资源中获取指向文件数据的指针
-	const void* data = mem_res.data();
-	// 获取文件长度
-	size_t length = mem_res.size();
-	// 将文件数据拷贝到 vector<unsigned char>
-	std::vector<unsigned char> result;
-	result.reserve(length);
-	std::copy_n(
-		reinterpret_cast<const unsigned char*>(data),
-		length,
-		std::back_inserter(result)
-	);
-	// 文件内存映射会在内存资源析构时自动解除
-	return result;
-}
-KrnlnApi std::vector<unsigned char> 读入文件(const std::wstring& filename) {
-	// 创建一个内存资源来映射文件
-	std::pmr::monotonic_buffer_resource mem_res;
-	std::pmr::map_file(filename, mem_res);
 
-	// 从内存资源中获取指向文件数据的指针
-	const void* data = mem_res.data();
-	// 获取文件长度
-	size_t length = mem_res.size();
-
-	// 将文件数据拷贝到 vector<unsigned char>
-	std::vector<unsigned char> result;
-	result.reserve(length);
-	std::copy_n(reinterpret_cast<const unsigned char*>(data), length,
-		std::back_inserter(result));
-
-	// 文件内存映射会在内存资源析构时自动解除
-	return result;
-}
-#else
 KrnlnApi std::vector<unsigned char> 读入文件(const std::string& filename) {
 	std::ifstream file(filename, std::ios::binary);
 	std::vector<unsigned char> data;
@@ -86,4 +41,3 @@ KrnlnApi std::vector<unsigned char> 读入文件(const std::wstring& filename) {
 	file.close();
 	return data;
 }
-#endif
